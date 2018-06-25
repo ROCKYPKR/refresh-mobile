27498f83616b01a6705f74820aa639d94c8d69aa
Adrian Czuczka <adrianczuczka@gmail.com>
2018-06-25 11:18:55 +0200

    Generated new files for JSON parsing

---
+++ lib/widgets/events/event_preview.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_preview.dart';
+
+// **************************************************************************
+// Generator: JsonSerializableGenerator
+// **************************************************************************
+
+EventData _$EventDataFromJson(Map<String, dynamic> json) => new EventData(
+    json['name'] as String,
+    json['start'] as String,
+    json['end'] as String,
+    json['location'] as String,
+    json['description'] as String);
+
+abstract class _$EventDataSerializerMixin {
+  String get name;
+  String get start;
+  String get end;
+  String get location;
+  String get description;
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'name': name,
+        'start': start,
+        'end': end,
+        'location': location,
+        'description': description
+      };
+}
---
+++ lib/widgets/shows/podcast.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'podcast.dart';
+
+// **************************************************************************
+// Generator: JsonSerializableGenerator
+// **************************************************************************
+
+PodcastData _$PodcastDataFromJson(Map<String, dynamic> json) => new PodcastData(
+    json['title'] as String,
+    json['description'] as String,
+    json['uri'] as String,
+    json['broadcast_date'] as String);
+
+abstract class _$PodcastDataSerializerMixin {
+  String get title;
+  String get description;
+  String get uri;
+  String get broadcastDate;
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'title': title,
+        'description': description,
+        'uri': uri,
+        'broadcast_date': broadcastDate
+      };
+}
