95a8d210540ab0e96085f34c58cc863e5ff9742b
Adrian Czuczka <adrianczuczka@gmail.com>
2018-09-22 16:41:45 +0100

    Implemented basic model of streaming now page. Also upgraded gradle and kotlin version.

--- lib/widgets/streaming_now/streaming_now_page.dart
+++ lib/widgets/streaming_now/streaming_now_page.dart
@@ -1,17 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:audio_stream_player/audio_stream_player.dart';
+import 'package:fresh_air/cross_platform_wrappers/platform_circular_indicator.dart';
+import 'package:fresh_air/helpers/website_api.dart';
+import 'package:fresh_air/widgets/shows/show_details.dart';
+import 'package:fresh_air/widgets/shows/show_preview.dart';
 
 class StreamingNowPage extends StatefulWidget {
-  StreamingNowPage({Key key}) : super(key : key);
+  StreamingNowPage({Key key}) : super(key: key);
 
   @override
   _StreamingNowPageState createState() => new _StreamingNowPageState();
 }
 
 class _StreamingNowPageState extends State<StreamingNowPage> {
+  AudioStreamPlayer player = AudioStreamPlayer();
+  bool isPlaying = false;
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Text("https://studio.freshair.org.uk:8443/radio"),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(top: 20.0),
+              child: FutureBuilder<List<ShowData>>(
+                future: WebsiteAPI.getAllShows(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) print(snapshot.error);
+                  if (snapshot.hasData) {
+                    var list = snapshot.data.map((item) {
+                      return new ShowPreview(data: item);
+                    }).toList();
+                    return list[0];
+                  } else {
+                    return new Center(child: new PlatformCircularIndicator());
+                  }
+                },
+              ),
+            ),
+            flex: 2,
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.live_tv),
+                  onPressed: () {
+                    player.stop();
+                    play();
+                  },
+                  iconSize: 60.0,
+                ),
+                IconButton(
+                  icon: Icon(isPlaying
+                      ? Icons.pause_circle_filled
+                      : Icons.play_circle_filled),
+                  onPressed: () {
+                    isPlaying ? pause() : play();
+                  },
+                  iconSize: 100.0,
+                ),
+                IconButton(
+                  icon: Icon(Icons.stop),
+                  onPressed: () {
+                    player.stop();
+                  },
+                  iconSize: 60.0,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  void play() {
+    player.play("http://studio.freshair.org.uk:8000/radio");
+    setState(() {
+      isPlaying = true;
+    });
+  }
+
+  void pause() {
+    player.pause();
+    setState(() {
+      isPlaying = false;
+    });
   }
 }
